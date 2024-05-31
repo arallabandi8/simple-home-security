3d70355aa5d8eed285bf8bae3bbdcac10926f2bf
arallabandi <152656376+arallabandi8@users.noreply.github.com>
2024-05-31 13:13:35 -0500

    Update network_credentials.h

--- homesecurity/network_credentials.h
+++ homesecurity/network_credentials.h
@@ -1,3 +1,3 @@
-#define SSID "home321" // network name
-#define PASS "Perm4ever" // network passwd
+#define SSID "myWiFiNetwork" // network name
+#define PASS "myPassword" // network passwd
 #define DST 1 // 1 if daylight savings active, 0 if not
